76738572a9159057d13504a2bdc1d07710e9e983
yebingyue <yebingyue@xiaohongshu.com>
2026-04-28 10:37:45 +0800

    chore: release grab v0.2.10

--- Formula/android-ui-grab.rb
+++ Formula/android-ui-grab.rb
@@ -1,9 +1,9 @@
 class AndroidUiGrab < Formula
   desc "Android UI Grab CLI"
   homepage "https://github.com/git54496/android-ui-grab"
-  url "https://github.com/git54496/android-ui-grab/archive/refs/tags/v0.2.9.tar.gz"
-  version "0.2.9"
-  sha256 "9f28b1366ac71c913d1f9f5cd1fab94aa15e1234b94e332ae03d5e24178a4433"
+  url "https://github.com/git54496/android-ui-grab/archive/refs/tags/v0.2.10.tar.gz"
+  version "0.2.10"
+  sha256 "d49bc2db386c590faa1e82091be024143268b93f78311e40fd8090216fa9296c"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/git54496/android-ui-grab.git", branch: "main"
@@ -34,7 +34,7 @@ class AndroidUiGrab < Formula
 
   test do
     version_output = shell_output("#{bin}/grab --version").strip
-    assert_equal "0.2.9", version_output
+    assert_equal "0.2.10", version_output
     output = shell_output("#{bin}/grab list")
     assert_match "\"success\": true", output
   end
